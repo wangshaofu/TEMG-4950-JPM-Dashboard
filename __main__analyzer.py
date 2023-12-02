d6ed05fa632bc599b1190f9162cba827c513669b
Shao-Fu-Wang <michaelwang920324@gmail.com>
2023-12-02 18:58:36 +0800

    feat: Finalize query modes.

--- __main__analyzer.py
+++ __main__analyzer.py
@@ -10,8 +10,9 @@ def filter_function(mode, query, report_quarter):
         specific_table = full_filing_info_table[full_filing_info_table['ETF Security Description'] == query]
     elif mode == 'Issuer':
         # find the specific issuer
-        specific_table = full_filing_info_table[full_filing_info_table['Issuer'] == query]
-    # find the largest holder
+        specific_table = full_filing_info_table[(full_filing_info_table['Issuer'] == query)]
+    elif mode == 'Asset Class':
+        specific_table = full_filing_info_table[full_filing_info_table['Asset Class'] == query]
     sum_by_company = specific_table.groupby('Company Name')['VALUE'].sum().reset_index().sort_values('VALUE',
                                                                                                      ascending=False)
     total_value = sum_by_company['VALUE'].sum()
@@ -23,7 +24,7 @@ def filter_function(mode, query, report_quarter):
 if __name__ == '__main__':
     report_quarter = input('Enter period of report(in format of 2023-Q1): ')
     get_date_from_period = translate_from_q_to_date(report_quarter)
-    search_mode = input('Enter search mode(1 for specific ETF, 2 for specific issuer): ')
+    search_mode = input('Enter search mode(1 for specific ETF, 2 for specific issuer, 4 for asset class): ')
     print('The query `full list available in TickerMeta.csv')
     if search_mode == '1':
         mode = 'ETF'
@@ -31,6 +32,9 @@ if __name__ == '__main__':
     elif search_mode == '2':
         mode = 'Issuer'
         query = input('Enter the specific Issuer to find(full list available in TickerMeta.csv): ')
+    elif search_mode == '3':
+        mode = 'Asset Class'
+        query = input('Enter the specific Asset Class to find: ')
     else:
         print('Bad search mode')
         exit(0)
